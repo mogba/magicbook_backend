5c73c359d22469362aa514be0240ee50abdd97b4
Gabriel de Morais <gabrieldemorais6@gmail.com>
2024-09-27 00:36:01 -0300

    feat: add deploy required files

--- magicbook_backend/settings.py
+++ magicbook_backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-@aigf^&(8i2!(@l^oj7b*t!nrtu&af2k#%bi0#yr18*(b8q6jy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '.vercel.app', '.now.sh']
 
 
 # Application definition
@@ -116,6 +116,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR/'static',]
+STATIC_ROOT = BASE_DIR/'staticfiles'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
