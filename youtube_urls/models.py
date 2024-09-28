04da3da83563a177c7700c051b81bdda1896b560
Gabriel de Morais <gabrieldemorais6@gmail.com>
2024-09-28 17:18:26 -0300

    feat(youtube_urls): add url model

--- youtube_urls/models.py
+++ youtube_urls/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Url(models.Model):
+    url = models.CharField("URL", max_length=100)
+    date_created = models.DateTimeField("Date created", auto_now_add=True)
+
+    def __str__(self):
+      return self.url
