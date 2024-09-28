9a3265ebaa46ce6560b6317558c832a097e61b46
Gabriel de Morais <gabrieldemorais6@gmail.com>
2024-09-28 17:18:06 -0300

    feat: add youtube_urls to INSTALLED_APPS

--- magicbook_backend/settings.py
+++ magicbook_backend/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'youtube_urls.apps.YoutubeUrlsConfig',
 ]
 
 MIDDLEWARE = [
