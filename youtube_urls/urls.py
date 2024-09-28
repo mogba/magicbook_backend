aeffef3ea495191ee0fb0276641298bae705182c
Gabriel de Morais <gabrieldemorais6@gmail.com>
2024-09-28 20:51:08 -0300

    feat(youtube_urls): add crud views

--- youtube_urls/urls.py
+++ youtube_urls/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 
 from . import views
 
+app_name = "youtube_urls"
 urlpatterns = [
     path("", views.index, name="index"),
+    path("<int:id>/", views.detail, name="detail"),
+    path("save/", views.save, name="create"),
+    path("save/<int:id>/", views.save, name="update"),
+    path("delete/<int:id>/", views.delete, name="delete"),
 ]
--- youtube_urls/views.py
+++ youtube_urls/views.py
@@ -1,6 +1,33 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
 
-# Create your views here.
+from .models import Url
+
 def index(request):
-  return HttpResponse("Hello, world! You're at youtube-urls.")
+  urls = Url.objects.all()
+  return render(request, "youtube_urls/index.html", {"urls": urls})
+
+def detail(request, id):
+  url = get_object_or_404(Url, pk = id)
+  return render(request, "youtube_urls/detail.html", {"url": url})
+
+def save(request, id = None):
+  try:
+    new_url = request.POST["url"]
+  except KeyError:
+    return render(request, "youtube_urls/index.html", {"error_message": "No URL provided."})
+
+  if (id is not None):
+    url = get_object_or_404(Url, pk = id)
+    url.url = new_url
+  else:
+    url = Url(url = new_url)
+  
+  url.save()
+  return HttpResponseRedirect(reverse("youtube_urls:index"))
+
+def delete(_, id):
+  url = get_object_or_404(Url, pk = id)
+  url.delete()
+  return HttpResponseRedirect(reverse("youtube_urls:index"))
