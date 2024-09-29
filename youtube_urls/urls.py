557a2bba6f281442cccdbb16d2cbb63d42d943a0
Gabriel de Morais <gabrieldemorais6@gmail.com>
2024-09-28 22:25:50 -0300

    feat(youtube_url): validated url before saving

--- youtube_urls/urls.py
+++ youtube_urls/urls.py
@@ -6,7 +6,7 @@ app_name = "youtube_urls"
 urlpatterns = [
     path("", views.index, name="index"),
     path("<int:id>/", views.detail, name="detail"),
-    path("save/", views.save, name="create"),
-    path("save/<int:id>/", views.save, name="update"),
+    path("save/", views.create, name="create"),
+    path("save/<int:id>/", views.update, name="update"),
     path("delete/<int:id>/", views.delete, name="delete"),
 ]
--- youtube_urls/views.py
+++ youtube_urls/views.py
@@ -1,7 +1,10 @@
+import re
+
+from django.contrib import messages
 from django.http import HttpResponseRedirect
+from urllib.parse import urlparse, parse_qs
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
-from urllib.parse import urlparse, parse_qs
 
 from .models import Url
 
@@ -13,21 +16,42 @@ def detail(request, id):
   url = get_object_or_404(Url, pk = id)
   return render(request, "youtube_urls/detail.html", {"url": url})
 
-def save(request, id = None):
+def create(request):
+  return save(request, "index")
+
+def update(request, id):
+  return save(request, "detail", id)
+
+def save(request, view_name, id = None):
+  redirect_url = reverse(f"youtube_urls:{view_name}") if id is None else reverse(f"youtube_urls:{view_name}", kwargs = {"id": id})
+
   try:
     new_url = request.POST["url"]
+
+    if not new_url:
+      raise KeyError
   except KeyError:
-    return render(request, "youtube_urls/index.html", {"error_message": "No URL provided."})
+    messages.error(request, "No URL provided.")
+    return HttpResponseRedirect(redirect_url)
+  
+  # This app supports two YouTube URL formats:
+  # https://youtu.be/<Video ID>
+  # https://www.youtube.com/watch?v=<Video ID>
+  
+  # Regular expression for both formats
+  pattern = re.compile(
+    r"^(https:\/\/)(www\.)?(youtu\.be\/[A-Za-z0-9_-]{11}|youtube\.com\/watch\?v=[A-Za-z0-9_-]{11})$"
+  )
+  if pattern.match(new_url) is None:
+    messages.error(request, "Invalid URL format.")
+    return HttpResponseRedirect(redirect_url)
   
   parsed_url = urlparse(new_url)
   query_params = parse_qs(parsed_url.query)
-
-  # This addresses the following cases:
-  # https://youtu.be/<video_id>
-  # https://www.youtube.com/watch?v=<video_id>
-  video_id = query_params["v"][0] if "v" in query_params else parsed_url.path.lstrip("/")
   
-  if (id is not None):
+  video_id = query_params["v"][0] if "v" in query_params else parsed_url.path.lstrip("/")
+
+  if id is not None:
     url = get_object_or_404(Url, pk = id)
     url.url = new_url
     url.video_id = video_id
@@ -35,7 +59,7 @@ def save(request, id = None):
     url = Url(url = new_url, video_id = video_id)
   
   url.save()
-  return HttpResponseRedirect(reverse("youtube_urls:index"))
+  return HttpResponseRedirect(redirect_url)
 
 def delete(_, id):
   url = get_object_or_404(Url, pk = id)
