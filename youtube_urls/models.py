88fe403c4d328b57b5e8a1520adb0e45a269cd2d
Gabriel de Morais <gabrieldemorais6@gmail.com>
2024-10-01 03:51:49 -0300

    refactor(youtube_urls): add date_updated and user columns in Url model

--- youtube_urls/models.py
+++ youtube_urls/models.py
@@ -1,9 +1,11 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 class Url(models.Model):
     url = models.CharField("URL", max_length = 100)
     video_id = models.CharField("Video ID", max_length = 32)
-    date_created = models.DateTimeField("Date created", auto_now_add = True)
+    date_updated = models.DateTimeField("Date updated", auto_now = True)
+    user = models.ForeignKey(User, on_delete = models.CASCADE, blank = True, null = True)
 
     def __str__(self):
       return self.url
